7bf132ee3e78fee5e639116ab52ccf18b10eccab
HosamMohd <hosamkareem@gmail.com>
2012-08-29 22:21:02 -0600

    "font changing & adding the icon"

--- LunchList/src/csci498/lunchlist/LunchList.java
+++ LunchList/src/csci498/lunchlist/LunchList.java
@@ -1,17 +1,41 @@
 package csci498.lunchlist;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LunchList extends Activity {
-
+	Restaurant r = new Restaurant();
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lunch_list);
+        
+        Button save = (Button)findViewById(R.id.save);
+        
+        save.setOnClickListener(onSave);
+        
     }
-
+    
+    private View.OnClickListener onSave = new View.OnClickListener() {
+    	public void onClick(View c){
+    		EditText name = (EditText)findViewById(R.id.name);
+    		EditText address = (EditText)findViewById(R.id.addr);
+    		
+    		r.setName(name.getText().toString());
+    		r.setAddress(address.getText().toString());
+    	}
+    };
+		
+	public void onClick(View v) {
+			// TODO Auto-generated method stub
+			
+		}
+	
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_lunch_list, menu);
