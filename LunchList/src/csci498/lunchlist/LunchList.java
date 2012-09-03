49d8a199ee5d1e3a7d2734712b2e507608007a4f
HosamMohd <hosamkareem@gmail.com>
2012-09-03 14:46:44 -0600

    "T#5_steps(1-4) ~ Adapter & Row(icons - title - address) & getView() & RestaurantHolder"

--- LunchList/src/csci498/lunchlist/LunchList.java
+++ LunchList/src/csci498/lunchlist/LunchList.java
@@ -6,20 +6,24 @@ import java.util.List;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 //import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class LunchList extends Activity {
 	List <Restaurant> model = new ArrayList <Restaurant>();
-	ArrayAdapter <Restaurant> adapter = null;
+	RestaurantAdapter adapter = null;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,12 +36,14 @@ public class LunchList extends Activity {
         
         Spinner list = (Spinner)findViewById(R.id.restaurants);
         
-        adapter = new ArrayAdapter<Restaurant>(this,android.R.layout.simple_list_item_1,model);
+        adapter = new RestaurantAdapter();
+        
         AutoCompleteTextView textView = (AutoCompleteTextView)
                 findViewById(R.id.addr);
         
         list.setAdapter(adapter);
-		/*RadioGroup rg = (RadioGroup)findViewById(R.id.types);
+		
+        /*RadioGroup rg = (RadioGroup)findViewById(R.id.types);
 		for(int i=1;i<9;i++)
 		{
 			RadioButton rb = new RadioButton(getBaseContext());
@@ -45,13 +51,13 @@ public class LunchList extends Activity {
 			rb.setTextColor(Color.rgb(5*i, 5*i, 5*i));
 			rg.addView(rb);
 		}*/
+        
     }
     
     private View.OnClickListener onSave = new View.OnClickListener() {
     	public void onClick(View c){
     		Restaurant r = new Restaurant();
-    		r.setName("Foo");
-    		model.add(r);
+    		//model.add(r);
     		EditText name = (EditText)findViewById(R.id.name);
     		EditText address = (EditText)findViewById(R.id.addr);
     		
@@ -81,6 +87,66 @@ public class LunchList extends Activity {
     	
     };
     
+    class RestaurantAdapter extends ArrayAdapter<Restaurant>{
+    	RestaurantAdapter(){
+    		super(LunchList.this,android.R.layout.simple_list_item_1,model);
+    	}
+    		public View getView (int position, View convertView, ViewGroup parent){
+    			View row = convertView;
+    			
+    			if(row==null){
+    				LayoutInflater inflater = getLayoutInflater();
+    				
+    				row =  inflater.inflate(R.layout.row, null);
+    			}
+    			
+    			Restaurant r = model.get(position);
+    			((TextView)row.findViewById(R.id.title)).setText(r.getName());
+    			((TextView)row.findViewById(R.id.address)).setText(r.getAddress());
+    			
+    			ImageView icon = (ImageView) row.findViewById(R.id.icon);
+    			
+    			if (r.getType().equals("sit_down")){
+    				icon.setImageResource(R.drawable.ball_red);
+    			}
+    			else if (r.getType().equals("take_out")){
+    				icon.setImageResource(R.drawable.ball_yellow);
+    			} 
+    			else{
+    				icon.setImageResource(R.drawable.ball_green);
+    			}
+    			return (row);
+    		}
+    	
+    }
+    
+    static class RestaurantHolder {
+    	private TextView name = null;
+    	private TextView address = null;
+    	private ImageView icon = null;
+    	
+    	RestaurantHolder(View row){
+    		name = (TextView) row.findViewById(R.id.title);
+    		address = (TextView) row.findViewById(R.id.address);
+    		icon = (ImageView) row.findViewById(R.id.icon);
+    	}
+    	
+    	void populateFrom (Restaurant r){
+    		name.setText(r.getName());
+    		address.setText(r.getAddress());
+    	
+    		if (r.getType().equals("sit_down")){
+    			icon.setImageResource(R.drawable.ball_red);
+    		}
+    		else if (r.getType().equals("take_out")) { 
+    			icon.setImageResource(R.drawable.ball_yellow);
+    		}
+    		else {
+    			icon.setImageResource(R.drawable.ball_green); 
+    		}
+    	}
+    	
+    }
     
     
 		
