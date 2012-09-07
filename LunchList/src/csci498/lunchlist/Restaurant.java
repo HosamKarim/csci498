648cb227386f24d79aa331b29c545996d3ea0f2e
HosamMohd <hosamkareem@gmail.com>
2012-09-07 12:20:06 -0600

    "Adding notes to the Restaurant"

--- LunchList/src/csci498/lunchlist/Restaurant.java
+++ LunchList/src/csci498/lunchlist/Restaurant.java
@@ -7,6 +7,7 @@ public class Restaurant {
 	private String name = "";
 	private String address = "";
 	private String type="";
+	private String notes="";
 
 	public String getName() { 
 		return(name);
@@ -35,5 +36,12 @@ public class Restaurant {
 	
 	public void setType(String type) { 
 		this.type = type;
+	}
+	
+	public String getNotes(){
+		return(notes);
+	}
+	public void setNotes(String notes){
+		this.notes = notes;
 	}
 }
