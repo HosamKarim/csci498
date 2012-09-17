1f21720b2db502f4e3af39c8efb576d87b0ea4e3
HosamMohd <hosamkareem@gmail.com>
2012-09-17 16:41:47 -0600

    "T#11_step(2) Manage our Schema, adding RestaurantHolder"

--- LunchList/src/csci498/lunchlist/RestaurantHelper.java
+++ LunchList/src/csci498/lunchlist/RestaurantHelper.java
@@ -5,6 +5,7 @@ import android.database.SQLException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteQueryBuilder;
+import android.database.Cursor;
 
 class RestaurantHelper extends SQLiteOpenHelper {	
 	private static final String DATABASE_NAME="lunchlist.db"; private static final int SCHEMA_VERSION=1;
@@ -14,8 +15,13 @@ class RestaurantHelper extends SQLiteOpenHelper {
 	}
 	
 	@Override
-	public void onCreate(SQLiteDatabase db) { }
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL("CREATE TABLE restaurants (_id INTEGER PRIMARY KEY AUTOINCREMENT name TEXT, " +
+				"address TEXT, type TEXT, notes TEXT);");
+	}
 	
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) { }
+	  // no-op, since will not be called until 2nd schema
+	  // version exists
 	}
