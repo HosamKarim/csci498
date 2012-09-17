131c980cb3a9360b0324073441413404b41ff9bd
HosamMohd <hosamkareem@gmail.com>
2012-09-17 17:03:34 -0600

    "T#11_step(3) Remove Extraneous Code from LunchList"

--- LunchList/src/csci498/lunchlist/LunchList.java
+++ LunchList/src/csci498/lunchlist/LunchList.java
@@ -42,13 +42,11 @@ public class LunchList extends TabActivity {
 	DatePicker dPicker = null;
 	RadioGroup types = null;
 	Restaurant current = null;
-	int progress;
-	AtomicBoolean isActive = new AtomicBoolean(true);
-	
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
-    	requestWindowFeature(Window.FEATURE_PROGRESS);
+    	
         setContentView(R.layout.activity_lunch_list);
         
         name = (EditText)findViewById(R.id.name);
@@ -196,97 +194,4 @@ public class LunchList extends TabActivity {
     	
     }
     
-    private void doSomeLongWork(final int incr){
-    	runOnUiThread(new Runnable(){
-    		public void run() {
-    			progress+=incr;
-    			setProgress(progress);
-    		}
-    	});
-    	
-    	SystemClock.sleep(250);
-    }
-    
-    private Runnable longTask = new Runnable() {
-    	public void run() {
-    		for (int i=progress; i<10000 && isActive.get(); i+=200) {
-    			doSomeLongWork(200);
-    		}
-    		
-    		if (isActive.get()){
-    			runOnUiThread(new Runnable() {
-					@Override
-					public void run() {
-						setProgressBarVisibility(false);
-						progress = 0;
-					}
-				});
-    		}
-    		
-    	}
-    };
-    
-    // PAUSING and RESUMING the progress
-    public void onPause(){
-    	super.onPause();
-    	
-    	isActive.set(false);
-    }
-    
-    public void onResume(){
-    	super.onResume();
-    	
-    	isActive.set(true);
-    	
-    	if (progress>0)
-    		startWork();
-    }
-		
-    public void startWork(){
-    	setProgressBarVisibility(true);
-    	new Thread(longTask).start();
-    }
-    
-	public void onClick(View v) {
-			// TODO Auto-generated method stub
-			
-		}
-	
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-    	new MenuInflater(this).inflate(R.menu.option, menu); 
-    	return(super.onCreateOptionsMenu(menu));
-    }
-    
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-    	if (item.getItemId()==R.id.toast) {
-    		String message="No restaurant selected";
-    		
-    		if (current!=null) { 
-    			message=current.getNotes();
-    		}
-    	
-    		Toast.makeText(this, message, Toast.LENGTH_LONG).show();
-    	
-    		return(true);
-    	}
-    	else if (item.getItemId()==R.id.run){
-    		startWork();
-    		
-    		return(true);
-    	}
-    	
-    	else if (item.getItemId()==R.id.change){
-    		if(getTabHost().getCurrentTabTag().equals("tag1"))
-    			getTabHost().setCurrentTab(1);
-    		else if(getTabHost().getCurrentTabTag().equals("tag2"))
-    			getTabHost().setCurrentTab(0);	
-    	}
-    	
-    	else if(item.getItemId()==R.id.clear){
-    		adapter.clear();
-    	}
-    	
-    	return(super.onOptionsItemSelected(item)); }
 }
