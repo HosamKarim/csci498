df2f4dacdfde8abb1068ac63f2fa992c22f094ea
HosamMohd <hosamkareem@gmail.com>
2012-10-05 21:13:41 -0600

    Coding the onHandleIntent method so that the Service get Feed fetching and parsing

--- LunchList/src/csci498/lunchlist/FeedService.java
+++ LunchList/src/csci498/lunchlist/FeedService.java
@@ -1,9 +1,14 @@
 package csci498.lunchlist;
+import org.mcsoxford.rss.RSSFeed;
+import org.mcsoxford.rss.RSSReader;
+
 import android.app.IntentService;
 import android.content.Intent;
+import android.util.Log;
 
 
 public class FeedService extends IntentService {
+	public static final String EXTRA_URL = "csci498.lunchlist.EXTRA_URL";
 	
 	public FeedService() {
 		super("FeedService");
@@ -11,7 +16,12 @@ public class FeedService extends IntentService {
 
 	@Override
 	protected void onHandleIntent(Intent i) {
-		//doSomething
+		RSSReader reader = new RSSReader();
 		
+		try {
+			RSSFeed result = reader.load(i.getStringExtra(EXTRA_URL));
+		} catch (Exception e) {
+			Log.e(getString(R.string.app_name),"Exception parsing feed",e);
+		}
 	}
 }
