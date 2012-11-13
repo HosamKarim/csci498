5fc1933cd815251145adb2340238ea94141cd906
HosamMohd <hosamkareem@gmail.com>
2012-11-12 22:46:55 -0700

    APT#24_step(2): Modifying the onResturantSelected() method to detect the large landscape mode

--- LunchList/src/csci498/lunchlist/LunchList.java
+++ LunchList/src/csci498/lunchlist/LunchList.java
@@ -3,25 +3,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 public class LunchList extends FragmentActivity 
-	implements LunchFragment.OnRestaurantListener {
-	
+implements LunchFragment.OnRestaurantListener {
+
 	public final static String ID_EXTRA = "csci498.lunchlist._ID";
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_lunch_list);
-		
+
 		LunchFragment lunch = (LunchFragment)getSupportFragmentManager()
 				.findFragmentById(R.id.lunch);
-		
+
 		lunch.setOnRestaurantListener(this);
 	}
-	
+
 	public void onRestaurantSelected(long id) {
-		Intent i = new Intent(this, DetailForm.class);
-		
-		i.putExtra(ID_EXTRA, String.valueOf(id));
-		startActivity(i); 
+		if(findViewById(R.id.details) == null) {
+			Intent i = new Intent(this, DetailForm.class);
+
+			i.putExtra(ID_EXTRA, String.valueOf(id));
+			startActivity(i);
 		}
+		else {
+
+		}
+	}
 }
